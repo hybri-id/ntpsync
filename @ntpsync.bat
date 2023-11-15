9d3626fd62585a40b257845945405c14b8ac9fe4
Hybrii-D <88566214+hybri-id@users.noreply.github.com>
2023-11-15 11:47:28 +0100

    Add files via upload

---
+++ @ntpsync.bat
@@ -0,0 +1,32 @@
+@echo off
+
+if not "%1"=="am_admin" (
+	powershell -Command "Start-Process -Verb RunAs -FilePath '%0' -ArgumentList 'am_admin'"
+	exit /b
+)
+
+tasklist /FI "IMAGENAME eq w32tm" 2>NUL | find /I /N "w32time">NUL
+if "%ERRORLEVEL%"=="0" (
+	goto s2
+) else (
+	goto continue
+)
+
+:continue
+net stop w32time
+w32tm /unregister
+w32tm /register
+
+:: Sync system clock with ntp server
+:s2
+net start w32time
+w32tm /resync /nowait
+w32tm /query /peers
+
+
+IF ERRORLEVEL 0 SET M=0
+IF ERRORLEVEL 1 SET M=1
+	if %M%==0 (
+		echo OK
+	) else echo "No se ha podido sincronizar el reloj" && pause
+exit
