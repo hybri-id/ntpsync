84e3e58fd541142e293d0603f8f00a7ac1c21c08
Hybrii-D <88566214+hybri-id@users.noreply.github.com>
2023-11-20 13:14:21 +0100

    Update ntp_sync.bat

--- ntp_sync.bat
+++ ntp_sync.bat
@@ -16,6 +16,14 @@ if "%ERRORLEVEL%"=="0" (
 
 ::Stop the service if it's running
 :continue
+echo Sync computer time from internet
+echo.
+echo Back-up registry w32time\Config
+reg export HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\services\w32time\Config exported_w32time.reg /y
+rem changing the registry keys temporarly:
+reg add HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\services\w32time\Config /v MaxNegPhaseCorrection /d 0xFFFFFFFF /t REG_DWORD /f
+reg add HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\services\w32time\Config /v MaxPosPhaseCorrection /d 0xFFFFFFFF /t REG_DWORD /f
+echo.
 net stop w32time
 w32tm /unregister
 w32tm /register
@@ -23,9 +31,18 @@ w32tm /register
 :: Start&Sync system clock with ntp server
 :not_running
 net start w32time
-w32tm /resync /nowait
+w32tm /config /syncfromflags:manual /manualpeerlist:time.nist.gov /reliable:yes /update
+echo.
+w32tm /resync /nowait /rediscover
+echo.
 w32tm /query /peers
+echo.
+w32tm /monitor
+echo.
+echo Restore registry w32time\Config
+reg import exported_w32time.reg
 
+pause
 
 IF ERRORLEVEL 0 SET M=0
 IF ERRORLEVEL 1 SET M=1
